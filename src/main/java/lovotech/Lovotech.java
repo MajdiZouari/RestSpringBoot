0ef907873b521759dd3b32b02b383e7f2e3844ab
jenkins <jenkins@sarbacane.com>
2018-02-23 21:39:24 +0100

    Add lombok library

--- src/main/java/lovotech/Lovotech.java
+++ src/main/java/lovotech/Lovotech.java
@@ -1,21 +1,13 @@
 package lovotech;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class Lovotech {
 	
     private final long id;
     private final String content;
 
-    public Lovotech(long id, String content) {
-        this.id = id;
-        this.content = content;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-	
 }
